1b44abf6903190cd873ea7eea6f4cfaa5c5da529
Paulo <paulo.ricardo1137.pr@gmail.com>
2024-11-25 08:16:56 +0000

    Welcome Function created

---
+++ blood_type.py
@@ -0,0 +1,19 @@
+
+# The welcome_display function introduces the program and provides to user with menu options.
+# The print are used to show text to the user.
+# The "-" * 35 creates a separator line for better readability.
+def welcome_display():
+    print('[', '='* 35, ']')
+    print('   Welcome to Blood Type Compatibily!')
+    print('[','='* 35, ']')
+    print(' ')
+    print("Choose an option:")
+    print(' ')
+    print("1. Check who can donate to your blood type.")
+    print("2. Check who you can donate blood to.")
+    print("3. Exit the program.")
+
+
+
+first = welcome_display()
+
